2289cf0742781410eae14a6ee117084255886290
Gregory John Casamento <greg.casamento@gmail.com>
2023-06-26 16:24:28 -0400

    Add files

--- NSURLConnection_test/main.m
+++ NSURLConnection_test/main.m
@@ -7,10 +7,23 @@
 
 #import <Foundation/Foundation.h>
 
+@interface TestDelegate : NSObject <NSURLConnectionDelegate>
+@end
+
+@implementation TestDelegate
+@end
+
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+    TestDelegate *d = [[TestDelegate alloc] init];
+    NSURL *u = [NSURL
+                URLWithString:
+                  @"http://[2600:1f18:2608:2820:ba4:d73d:5e3:5ca3]:4444/wd/hub/status"];
+    NSURLRequest *r = [NSURLRequest requestWithURL:u];
+    NSURLConnection *conn = [NSURLConnection connectionWithRequest: r
+                                                          delegate: d];
+    // insert code here...
+    NSLog(@"Connection complete... %@", u);
   }
   return 0;
 }
